6a707f35982c2329e45a5c49cfb8b89d337b9976
Diaz <diazjuan@paho.org>
2018-12-04 18:53:18 -0500

    Ebola-Response: Reformat to current template in blue, Inclusion of variables catid, task, gid, inclusion of noindex, nofollow for malformed pages, Elimination of record of the canincal urls. inclusion of file functions, reconstruction of index.php to new format.

--- Ebola-response/index.php
+++ Ebola-response/index.php
@@ -1,66 +1,123 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' );
-$isIE = (bool) strpos($_SERVER['HTTP_USER_AGENT'],'MSIE');
-?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml"  lang="<?php echo $this->language; ?>" >
- 
+$isIE = FALSE;
+if (isset($_SERVER['HTTP_USER_AGENT'])) {
+	$isIE = (bool) strpos($_SERVER['HTTP_USER_AGENT'],'MSIE');
+}	
+
+include_once (dirname(__FILE__).'/functions.php');
+global $tpl;
+$tpl = new Joomla_Template($this);
+
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+
+$this->language = $doc->language;
+$this->direction = $doc->direction;
+
+$this->setGenerator(null);
+
+?><!DOCTYPE html>
+<html xmlns="https://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-<?php
-	if ($isIE) { ?>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<?php } ?>
+ 
 <?php //include ('canonical_tag.php'); ?>
-<jdoc:include type="head" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php
+
+$option   = $app->input->getCmd('option', '');
+$view   = $app->input->getCmd('view', '');
+$sitename = JFactory::getApplication()->get('sitename');
+//$sitename = JFactory::getApplication()->get('sitename');
+//$sitename = "PAHO/WHO"; // $app->getCfg('sitename');
+
+$JUribase = JURI::base();
+$JUribase = str_replace('http://www.paho.org','https://www.paho.org',$JUribase);
+	/* compresion de css */
+$compress_css = false;
+if ($compress_css) {
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/css-compress.php');
+} else {
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/bootstrap.css');
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/font-awesome.min.css');
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/font-awesome.css');
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/main.css');
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/custom.css');
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/paho.css');
+	$doc->addStyleSheet($JUribase . 'templates/' . $this->template . '/css/impl.css');
+}
+$doc->addScript('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', 'text/javascript');
+?>
+	<script type="text/javascript">
+		var pathInfo = {
+			base: '<?php echo (JFactory::getDocument()->getBase() . 'templates/' . $this->template); ?>/',
+			css: 'css/',
+			js: 'js/',
+			swf: 'swf/',
+		}
+	</script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- <meta name="theme-color" content="#0099d9" /> -->
+	<jdoc:include type="head" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://www.paho.org/images/icons/ios-ipad-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://www.paho.org/images/icons/ios-iphone-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" href="https://www.paho.org/images/icons/ios-default-homescreen-57x57.png" />
+	<meta http-equiv="refresh" content="28800">
+
 	<link rel="stylesheet" href="<?php echo JURI::base(); ?>templates/<?php echo $this->template?>/css/style.css" type="text/css" />
-	<!--[if lt IE 9]>
-	<link rel="stylesheet" href="<?php echo JURI::base(); ?>templates/<?php echo $this->template?>/css/style_ie.css" type="text/css" />
-	<![endif]-->
-	<link href='http://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="<?php echo JURI::base(); ?>templates/<?php echo $this->template?>/css/print.css" type="text/css" media="print" />
-	<script type="text/javascript" src="<?php echo JURI::base(); ?>media/system/js/language.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" ></script>
-	<script type="text/javascript" src="<?php echo JURI::base(); ?>templates/<?php echo $this->template?>/js/html5lightbox/html5lightbox.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script type="text/javascript">
+		jQuery(window).on('load', function () {
+		jQuery('iframe[id^=twitter-widget-]').each(function () {
+		var head = jQuery(this).contents().find('head');
+		if (head.length) {
+		head.append('<style type="text/css">.timeline { max-width: 100% !important; width: 100% !important; } .timeline .stream { max-width: none !important; width: 100% !important; }</style>');
+		}
+		jQuery('.twitter-timeline').append(jQuery(''));
+		})
+		});
+	</script>
+
 <?php
-	$lm = 0;
-	$lm = $lm + ($this->countModules( 'left1' )); 
-	$lm = $lm + ($this->countModules( 'left2' )); 
-	$lm = $lm + ($this->countModules( 'left3' )); 
-	$lm = $lm + ($this->countModules( 'left5' )); 
-	$rm = 0;
-	$rm = $rm + ($this->countModules( 'right' )); 
-	$rm = $rm + ($this->countModules( 'right1' )); 
-	$rm = $rm + ($this->countModules( 'right2' )); 
-	$rm = $rm + ($this->countModules( 'right3' )); 
-	$rm = $rm + ($this->countModules( 'right4' )); 
-	if (!$lm && !$rm) {
-		$content = " id=\"content_full\"";
-		$access  = "#content_full";
-	}
-	if ($lm && !$rm) {
-		$content = " id=\"content_right\"";
-		$access = "#content_right";
-	}
-	if (!$lm && $rm) {
-		$content = " id=\"content_left\"";
-		$access = "#content_left";
-	}
-	if ($lm && $rm) {
-		$content = " id=\"content_lr\"";
-		$access = "#content_lr";
-	}
-	if ($content == ' id="content_right"' || $content == ' id="content_full"') {
-?>
-	<style>
-	#main { background-image: none; }
-	</style>
-<?php
-	}
+
+$lm = 0;
+$lm = $lm + ($this->countModules( 'left1' )); 
+$lm = $lm + ($this->countModules( 'left2' )); 
+$lm = $lm + ($this->countModules( 'left3' )); 
+$lm = $lm + ($this->countModules( 'left5' )); 
+$rm = 0;
+$rm = $rm + ($this->countModules( 'right' )); 
+$rm = $rm + ($this->countModules( 'right1' )); 
+$rm = $rm + ($this->countModules( 'right2' )); 
+$rm = $rm + ($this->countModules( 'right3' )); 
+$rm = $rm + ($this->countModules( 'right4' )); 
+if (!$lm && !$rm) {
+	$content = " id=\"content_full\"";
+	$class = "col-xs-12";
+	$access  = "#content_full";
+}
+if ($lm && !$rm) {
+	$content = " id=\"content_right\"";
+	$class="col-lg-10 col-md-10 col-sm-9 col-xs-12";
+	$access = "#content_right";
+}
+if (!$lm && $rm) {
+	$content = " id=\"content_left\"";
+	$class="col-lg-10 col-md-9 col-sm-9 col-xs-12";
+	$access = "#content_left";
+}
+if ($lm && $rm) {
+	$content = " id=\"content_lr\"";
+	$class="col-lg-8 col-md-7 col-sm-6 col-xs-12";
+	$access = "#content_lr";
+}
+
 ?>
 </head>
-<body>
+<body itemscope itemtype="https://schema.org/WebPage">
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MDCJXB" title="Google Tag Manager"
 style="height:0px;width:0px;display:none;visibility:hidden"><span style="visibility:hidden">Google Tag</span></iframe></noscript>
@@ -70,38 +127,45 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MDCJXB');</script>
 <!-- End Google Tag Manager -->
- <?php
- $menu = & JSite::getMenu();
-if ($menu->getActive() == $menu->getDefault()) {
-	$conf =& JFactory::getConfig();
-	$sitename = $conf->getValue('config.sitename');
-	echo "<h1 style=\"display:none\">".$sitename,"</h1>"; 
-}
-?>
-<div id="wrapper">
-	<div id="access">
-			<p><a href="<?php echo $access; ?>" title="Skip to content">Skip to content</a></p>
-		</div><!-- end of #access -->
-		<div id="language">
-			<jdoc:include type="modules" name="language" />
-		</div><!-- end of #language -->
-		<div id="header">
-			<div id="banner">
-				<jdoc:include type="modules" name="banner" style="raw" />
-			</div><!-- end of #banner -->
-			<div id="flag">
-				<jdoc:include type="modules" name="flag" style="raw" />
-			</div><!-- end of #flag -->
-		</div><!-- end of #header -->
-		<div id="menus">
-			<p class="ocm"><img src="<?php echo JURI::base(); ?>templates/<?php echo $this->template?>/images/mobile_menu_icon.png" style="width: 17px;float:left;margin-right:4px;border:0" alt="" />Menu</p>
-			<div id="open_close_menu">
-				<jdoc:include type="modules" name="eb-menu" />
-			</div><!-- #open_close_menus -->
-			<div id="search">
-				<jdoc:include type="modules" name="user40" />
-			</div><!-- end of #search -->
-		</div><!-- end of #menus -->
+<!--googleoff: index-->  
+	<div id="wrapper">
+		<jdoc:include type="modules" name="sidebar_social" />
+		<div class="container">
+			<div class="row">
+			  <header id="header">
+				<div class="col-xs-12">
+				  <nav class="navbar navbar-default">
+					<div class="container-fluid">
+					  <div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						  <span class="sr-only">Toggle navigation</span>
+						  <span class="icon-bar"></span>
+						  <span class="icon-bar"></span>
+						  <span class="icon-bar"></span>
+						</button>
+								<div class="top_banner">
+									<jdoc:include type="modules" name="banners" style="raw" />
+								</div>
+								<jdoc:include type="modules" name="language_switcher_mobile" />
+					  </div>
+					  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<div class="topheader">
+									<div class="topsearch">
+						<jdoc:include type="modules" name="user4" />
+									</div>
+									<span class="lang-inline toplanguage">
+										<jdoc:include type="modules" name="language" />
+									</span>
+						<jdoc:include type="modules" name="share2" style="xhtml" />
+								</div>	
+						<jdoc:include type="modules" name="header" style="xhtml" />
+					  </div>
+					</div>
+				  </nav><!-- nav class="navbar navbar-default" -->
+				</div><!--  class="col-xs-12" -->
+			</header>
+			</div>
+<gcse:searchresults></gcse:searchresults>
 
 	<div id="main">
 		<div id="content-2"<?php if (!$this->countModules( 'eb-right' )) { echo ' style="width: 100%"'; } ?>><!-- content -->
@@ -109,7 +173,7 @@ if ($menu->getActive() == $menu->getDefault()) {
 			<div id="premain">
 				<jdoc:include type="modules" name="premain" style="xhtml" />
 			</div><!-- end of #premain -->
-<?php } ?>
+<?php } ?><!--googleon: index-->
 			<jdoc:include type="component" />
 
 			<div id="ct_left-1">
@@ -140,19 +204,20 @@ if ($menu->getActive() == $menu->getDefault()) {
 			</div><!-- end of #postcols -->
 <?php } ?>
 		</div><!-- #content -->
-<?php if ($this->countModules( 'share' ) || $this->countModules( 'eb-right' )) { ?>
+		<?php if ($this->countModules( 'eb-right' )) { ?>
 		<div id="ct_right">
-			<div id="share">
-				<jdoc:include type="modules" name="share" style="raw" />
-			</div><!-- end of #share -->
+			<div id="eb-share"><jdoc:include type="modules" name="share" style="xhtml" /></div>
+			<div class="clr"></div>
+			<jdoc:include type="modules" name="eb-menu" style="xhtml" />
 			<jdoc:include type="modules" name="eb-right" style="xhtml" />
 		</div><!-- end of #ct_right -->
 <?php } ?>
 	</div><!-- #main -->
 	<div style="clear:both"></div>
-</div><!-- #wrapper -->
-	<div id="footr_out">
-		<div id="footr_in">
+			<footer id="footer">
+			  <div class="footer-holder row">
+				<div class="col-xs-12">
+				<div id="footr_in">
 			<div id="honcode">
 				<jdoc:include type="modules" name="footer_left" />
 			</div><!-- end of #honcode -->
@@ -165,20 +230,32 @@ if ($menu->getActive() == $menu->getDefault()) {
 			<div class="ftmenu">
 				<jdoc:include type="modules" name="ftmenu3" style="xhtml" />
 			</div><!-- end of .ftmenu -->
-		</div><!-- end of #footr_in -->
-		<div id="site-info">
+				
+				</div><!-- end of #footr_in -->
+				</div><!-- end of col-xs-12 -->
+			  </div>
+			  <div class="holder row hidden-xs">
+				<div class="col-xs-12">
 			<jdoc:include type="modules" name="site_info" style="raw" />
-		</div><!-- end of #site-info -->
-		<div class="clr"></div>
-	</div><!-- end of #footr_out -->
+				</div><!-- end of col-xs-12 -->
+			  </div><!-- end of holder row hidden-xs -->
+			</footer><!-- end of footer-holder row -->
+		</div>
+		</div><!-- #wrapper -->
+
+		<jdoc:include type="modules" name="debug" />
+	</div>
+<script src="<?php echo $JUribase . 'templates/' . $this->template . '/js/bootstrap.min.js'; ?>"></script>
+<script src="<?php echo $JUribase . 'templates/' . $this->template . '/js/jquery.main.js'; ?>"></script>
+
+<script>/* alert(jQuery('.container').width()); */</script> 
 	<script>
-	$(document).ready(function(){
-		$(".ocm").click(function(){
-			$("#open_close_menu").toggle(500);
-		});
-		inicio();
-		$(window).resize(function() {
-			reinicio();
+	jQuery(document).ready(function(){
+	   if (jQuery('#big_slide').length){
+			inicio();   	     
+    	}
+		jQuery(".ocm").click(function(){
+			jQuery("#open_close_menu").toggle(500);
 		});
 	});
 	</script>
@@ -192,5 +269,14 @@ if ($menu->getActive() == $menu->getDefault()) {
   ga('send', 'pageview');
 
 </script>
+<script>
+if ( jQuery("#dcslider").length ) {
+	inicio();
+	jQuery(window).resize(function() {
+		reinicio();
+	});
+}
+</script>
+<!--googleon: index-->
 </body>
 </html>
