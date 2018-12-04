b32457607830a13844ed10cb3272e8e83ab98c00
Diaz <diazjuan@paho.org>
2018-12-04 18:31:59 -0500

    Inclusion of template Internal

---
+++ Internal/html/com_content/featured/default_item.php
@@ -0,0 +1,173 @@
+<?php
+defined('_JEXEC') or die;
+$params = &$this->item->params;
+$images = json_decode($this->item->images);
+$canEdit	= $this->item->params->get('access-edit');
+$info    = $this->item->params->get('info_block_position', 0);
+?>
+<?php if ($params->get('show_print_icon') || $params->get('show_email_icon') || $canEdit) : ?>
+<div class="btn-group pull-right"> <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <i class="icon-cog"></i><span class="caret"></span> </a>
+	<ul class="dropdown-menu">
+		<?php if ($params->get('show_print_icon')) : ?>
+			<li class="print-icon"> <?php echo JHtml::_('icon.print_popup', $this->item, $params); ?> </li>
+		<?php endif; ?>
+		<?php if ($params->get('show_email_icon')) : ?>
+			<li class="email-icon"> <?php echo JHtml::_('icon.email', $this->item, $params); ?> </li>
+		<?php endif; ?>
+		<?php if ($canEdit) : ?>
+			<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $this->item, $params); ?> </li>
+		<?php endif; ?>
+	</ul>
+</div>
+<?php endif; ?>
+
+<?php if ($params->get('show_title')) : ?>
+
+<div class="col-md-6">
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="textholder">
+				<h1><?php echo JText::_( 'LATEST_NEWS' ); ?></h1>
+	<h2>
+	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); ?>"> <?php echo $this->escape($this->item->title); ?></a>
+	<?php else : ?>
+		<?php echo $this->escape($this->item->title); ?>
+	<?php endif; ?>
+	</h2>
+<?php endif; ?>
+
+<?php if ($this->item->state == 0): ?>
+	<span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
+<?php endif; ?>
+
+<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
+	<small class="createdby">
+	<?php $author = $this->item->author; ?>
+	<?php $author = ($this->item->created_by_alias ? $this->item->created_by_alias : $author); ?>
+	<?php if (!empty($this->item->contactid ) && $params->get('link_author') == true) : ?>
+		<?php
+		echo JText::sprintf('COM_CONTENT_WRITTEN_BY',
+			JHtml::_('link', JRoute::_('index.php?option=com_contact&view=contact&id='.$this->item->contactid), $author)
+		); ?>
+		<?php else :?>
+			<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+		<?php endif; ?>
+	</small>
+<?php endif; ?>
+
+<?php $useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date')
+	|| $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category')); ?>
+	
+<?php if ($useDefList && ($info == 0 ||  $info == 2)) : ?>
+	<div class="article-info muted">
+		<dl class="article-info">
+		<dt class="article-info-term">
+			<?php echo JText::_('COM_CONTENT_ARTICLE_INFO'); ?>
+		</dt>
+
+		<?php if ($params->get('show_parent_category') && !empty($this->item->parent_slug)) : ?>
+			<dd>
+				<div class="parent-category-name">
+					<?php $title = $this->escape($this->item->parent_title);
+					$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->parent_slug)).'">'.$title.'</a>';?>
+					<?php if ($params->get('link_parent_category') && !empty($this->item->parent_slug)) : ?>
+						<?php echo JText::sprintf('COM_CONTENT_PARENT', $url); ?>
+					<?php else : ?>
+						<?php echo JText::sprintf('COM_CONTENT_PARENT', $title); ?>
+					<?php endif; ?>
+				</div>
+			</dd>
+		<?php endif; ?>
+		<?php if ($params->get('show_category')) : ?>
+			<dd>
+				<div class="category-name">
+					<?php $title = $this->escape($this->item->category_title);
+					$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)).'">'.$title.'</a>';?>
+					<?php if ($params->get('link_category') && $this->item->catslug) : ?>
+						<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $url); ?>
+					<?php else : ?>
+						<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $title); ?>
+					<?php endif; ?>
+				</div>
+			</dd>
+		<?php endif; ?>
+
+		<?php if ($params->get('show_publish_date')) : ?>
+			<dd>
+				<div class="published">
+					<i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $this->item->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
+				</div>
+			</dd>
+		<?php endif; ?>
+
+		<?php if ($info == 0): ?>
+			<?php if ($params->get('show_modify_date')) : ?>
+				<dd>
+					<div class="modified">
+						<i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $this->item->modified, JText::_('DATE_FORMAT_LC3'))); ?>
+					</div>
+				</dd>
+			<?php endif; ?>
+			<?php if ($params->get('show_create_date')) : ?>
+				<dd>
+					<div class="create">
+						<i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $this->item->created, JText::_('DATE_FORMAT_LC3'))); ?>
+					</div>
+				</dd>
+			<?php endif; ?>
+
+			<?php if ($params->get('show_hits')) : ?>
+				<dd>
+					<div class="hits">
+						<i class="icon-eye-open"></i> <?php echo JText::sprintf('COM_CONTENT_ARTICLE_HITS', $this->item->hits); ?>
+					</div>
+				</dd>
+			<?php endif; ?>
+		<?php endif; ?>
+		</dl>
+	</div>
+<?php endif; ?>
+
+<?php if (!$params->get('show_intro')) : ?>
+	<?php echo $this->item->event->afterDisplayTitle; ?>
+<?php endif; ?>
+
+<?php echo $this->item->event->beforeDisplayContent; ?> <?php echo $this->item->introtext; ?>
+
+<?php if ($params->get('show_readmore') && $this->item->readmore) :
+if ($params->get('access-view')) :
+	$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid));
+else :
+	$menu = JFactory::getApplication()->getMenu();
+	$active = $menu->getActive();
+	$itemId = $active->id;
+	$link1 = JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId);
+	$returnURL = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid));
+	$link = new JURI($link1);
+	$link->setVar('return', base64_encode($returnURL));
+endif;
+?>
+				<a href="<?php echo $link; ?>" class="more">Read More</a>
+			</div><!-- end of textholder -->
+		</div><!-- end of col-sm-6 -->
+		<div class="col-sm-6">
+      	<div class="img-holder">
+				<img
+		<?php if ($images->image_intro_caption):
+			echo 'class="caption"'.' title="' .htmlspecialchars($images->image_intro_caption) .'"';
+		endif; ?>
+		src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" width="310" height="299">
+			</div><!-- end of img-holder -->
+		</div><!-- end of col-sm-6 -->
+	</div><!-- end of row -->
+</div><!-- end of col-md-6 -->
+
+
+	
+<?php endif; ?>
+
+<?php if ($this->item->state == 0) : ?>
+	</div>
+<?php endif; ?>
+<?php echo $this->item->event->afterDisplayContent; ?>
---
+++ Internal/html/mod_Slideshow/default.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Slideshow Module Layout
+ * Created by Paulo Leite - Version December 2013
+ * @license        GNU/GPL, see LICENSE.php
+ * mod_slideshow is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+print_r($detalhes);
+return;
+
+?>
+<script type="text/javascript">
+var ctr = 0;
+texto = new Array();
+<?php
+$bigdiv = "<div id=\"big_slide\">\r\n";
+$img_margin = "";
+foreach ($detalhes as $row) {
+?>
+<?	$txto = str_replace("'", "&#39;", $row->title); ?>
+<?	$txto = str_replace('"', "&quot;", $txto); ?>
+<?	$txto = '<p><a href="' . $row->link . '">' . $txto . "</a></p>"; ?>
+	texto[ctr] = '<?php echo $txto; ?>';
+	ctr++;
+<?php
+	if (property_exists($row, 'image')) {  
+		$bigdiv .= "\t".'<div class="fp_slsh_foto"><div style="position:relative"><a href="' . $row->link . '"><img src="' . $row->image . '" alt="' . substr($row->title,0,50) . '..." /></a><div class="mascara"><img src="modules/'.$module->module . '/images/mascara.png" alt=" " /></div></div>'."\r\n".'<!-- end of .fp_slsh_foto --></div>'."\r\n".'<div class="fp_slsh_texto">'."\r\n".'<div class="sl_texto"><a href="' . $row->link . '"><h2>' . $row->title . '</h2></a><p><a href="' . $row->link . '">' .  JText::_("Read more...",$jsSafe = true) . '</a></p></div>'."\r\n".'<!-- end of .sl_texto -->'."\r\n".'</div>'."\r\n".'<!-- end of .fp_slsh_texto -->'."\r\n";
+	}
+	if (property_exists($row, 'video')) {  
+		if ($row->flag) { $img_margin = '_margin'; }
+		$bigdiv .= "<div class=\"fp_slsh_foto".$img_margin."\"><div style=\"position:relative\"><img src=\"" . $row->thumb . "\" alt=\"" . substr($row->title,0,50) . "...\" /><div class=\"mascara".$img_margin."\"><img src=\"modules/".$module->module . "/images/mascara.png\" alt=\" \" /></div><div class=\"fp_slsh_play".$img_margin."\"><a href=\"" . $row->ytlink . "\" class=\"html5lightbox\"><img src=\"modules/".$module->module . "/images/yt_play-600.png\" style=\"border: none\" alt=\"Play\" /></a></div><!-- end of .fp_slsh_play --></div><!-- end of .fp_slsh_foto --></div>"."\r\n".""."\r\n"."<div class=\"fp_slsh_texto\">"."\r\n"."<div class=\"sl_texto\"><a href=\"" . $row->link . "\"><h2>" . $row->title . "</h2></a><p><a href=\"" . $row->link . "\">" .  JText::_("Read more...",$jsSafe = true) . "</a></p></div><!-- end of .sl_texto --></div>"."\r\n"."<!-- end of .fp_slsh_texto -->\r\n";
+		$img_margin = '';
+	}
+}
+echo "</script>\n";
+if (property_exists($detalhes[0], 'image')) {  
+	$bigdiv .= '<div class="fp_slsh_foto"><div style="position:relative"><img src="' . $detalhes[0]->image . '" alt="'.substr($detalhes[0]->title,0,50) . '..." /><div class="mascara"><img src="modules/'.$module->module . '/images/mascara.png" alt=" " /></div></div><!-- end of .fp_slsh_foto --></div>'."\r\n".'<div class="fp_slsh_texto">'."\r\n".'<div class="sl_texto"><a href="' . 			$detalhes[0]->link . '"><h2>' . $detalhes[0]->title . '</h2></a><p><a href="' . $detalhes[0]->link . '">' .  JText::_("Read more...",$jsSafe = true) . '</a></p></div><!-- end of .sl_texto --></div><!-- end of .fp_slsh_texto -->';
+}
+if(property_exists($detalhes[0], 'video')) {
+	if ($detalhes[0]->flag) { $img_margin = '_margin'; }
+	$bigdiv .= "<div class=\"fp_slsh_foto".$img_margin."\"><div style=\"position:relative\"><img src=\"" . $detalhes[0]->thumb . "\" alt=\"" . substr($detalhes[0]->title,0,50) . "...\" /><div class=\"mascara".$img_margin."\"><img src=\"modules/".$module->module . "/images/mascara.png\"  alt=\" \" /></div><div class=\"fp_slsh_play".$img_margin."\"><a href=\"" . $detalhes[0]->ytlink . "\" class=\"html5lightbox\"><img src=\"modules/".$module->module . "/images/yt_play-600.png\" style=\"border: none\" alt=\"Play\" /></a></div><!-- end of .fp_slsh_play --></div><!-- end of .fp_slsh_foto --></div>"."\r\n".""."\r\n"."<div class=\"fp_slsh_texto\">"."\r\n"."<div class=\"sl_texto\"><a href=\"" . $detalhes[0]->link . "\"><h2>" . $detalhes[0]->title . "</h2></a><p><a href=\"" . $detalhes[0]->link . "\">" .  JText::_("Read more...",$jsSafe = true) . "</a></p></div><!-- end of .sl_texto --></div>"."\r\n"."<!-- end of .fp_slsh_texto -->\r\n";
+	$img_margin = '';
+}
+	$bigdiv .="</div><!-- end of #big_slide -->\n";
+?>
+<div id="slides">
+<?php 
+	// cambio jc 20140311 - Reemplazo de & por &amp; para mejorar validación HTML
+	$bigdiv = str_replace('&', '&amp;', $bigdiv);
+	$bigdiv = str_replace('&amp;amp;', '&amp;', $bigdiv);
+	echo $bigdiv; ?>
+	<div id="sld_crc"><div id="circles" style="width: <?php echo count($detalhes) * 21; ?>px">
+<?php
+		echo "<div id=\"circle0\" class=\"color-circle-b\"><a style=\"outline: none;border:none\" href=\"javascript:ssGoto('0',this);\"><img src=\"" . JURI::base() . "modules/" . $module->module . "/images/null.gif\" width=\"11\" alt=\"slide 0\" style=\"border:0px\" /></a></div>";
+	for ($i=1; $i < count($detalhes); $i++) {
+		echo "<div id=\"circle" . $i . "\" class=\"color-circle\"><a style=\"outline: none;border:none\" href=\"javascript:ssGoto('$i', this);\"><img src=\"" . JURI::base() . "modules/" . $module->module . "/images/null.gif\" width=\"11\" alt=\"slide $i\" style=\"border:0px\" /></a></div>";
+	}
+	?>
+</div><!-- end of #circles -->
+</div><!-- end of #sld_crc -->
+</div><!-- end of #slides -->
+<div id="mini_sld_texto"><p><a href="<?php echo $detalhes[0]->link; ?>"><?php echo $detalhes[0]->title; ?></a></p></div>
+<div class="clr"></div>
+<script> 
+function inicio() {
+	maxwdt = jQuery("#header").width();
+	comando = "";
+	inivlr = 0;
+	numslds = <?php echo count($detalhes); ?>;
+	totwdt = (<?php echo count($detalhes); ?>+1)*maxwdt;
+	tmp = 0;
+	ssInterval = setInterval( "moverfwd()", 7000 );
+}
+function reinicio() {
+	ssGoto(0);
+		jQuery("#big_slide").animate({left: '0px'},0);
+		clearInterval(ssInterval);
+		inivlr = maxwdt;
+	inicio();
+}
+function moverfwd() {
+	for (ix = 0; ix < <?php echo count($detalhes); ?>; ix++) {
+		document.getElementById("circle" + ix).style.background = "#F92";
+	}
+	tmp++;
+	if (tmp == numslds) tmp = 0;
+   document.getElementById('circle'+tmp).style.background = '#258';
+   document.getElementById('mini_sld_texto').innerHTML = texto[tmp];
+	inivlr = inivlr + maxwdt;
+	if (inivlr == totwdt) {
+		jQuery("#big_slide").animate({left: '0px'},0);
+		inivlr = maxwdt;
+	}
+	comando = '-' + inivlr + 'px';
+	jQuery("#big_slide").animate({left:comando},500);
+}
+function ssGoto(nbr) {
+	clearInterval(ssInterval);
+	for (ix = 0; ix < <?php echo count($detalhes); ?>; ix++) {
+		document.getElementById("circle" + ix).style.background = "#F92";
+	}
+	document.getElementById("circle" + nbr).style.background = "#258";
+   document.getElementById('mini_sld_texto').innerHTML = texto[nbr];
+	nbr = nbr * maxwdt;
+	comando = '-' + nbr + 'px';
+	jQuery("#big_slide").animate({left:comando},500);
+}
+</script> 
